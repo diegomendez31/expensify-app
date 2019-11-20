1432d5fb0cc4e7c57d8f601a1baee1bb3fb6e874
diegomendez4131 <diego.mendez@alperform.com>
2019-11-20 16:09:00 +0000

    Add add expenses firebase

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,22 +1,9 @@
 import uuid from 'uuid';
-
+import database from '../firebase/firebase';
 // ADD_EXPENSE
-export const addExpense = (
-    { 
-        description = '', 
-        note='', 
-        amount=0, 
-        createAt=0
-    } = {}
-    ) => ({
+export const addExpense = (expense) => ({
     type: 'ADD_EXPENSE',
-    expense: {
-        id: uuid(),
-        description,
-        note,
-        amount,
-        createAt
-    }
+    expense
 });
 
 export const startAddExpense = (expenseData = {}) => {
@@ -27,6 +14,13 @@ export const startAddExpense = (expenseData = {}) => {
             amount=0, 
             createAt=0
         } = expenseData;
+        const expense = { description, note, amount, createAt}
+        database.ref('expenses').push(expense).then((ref) => {
+            dispatch(addExpense({
+                id: ref.key,
+                ...expense
+            }));
+        });
     };
 };
 
